8e80b7db43f0872779634cace914178d30dd6cfa
ankurvaseashta <root@USD1703230.local>
2018-06-26 10:07:12 -0400

    Cleanup

--- materialUI/src/pages/index.js
+++ materialUI/src/pages/index.js
@@ -69,60 +69,72 @@ class Index extends React.Component {
     const { classes } = this.props;
     const { open } = this.state;
 
-    return (
-      <div className={ classes.root }>
-        <Dialog open={ open } onClose={ this.handleClose }>
-          <DialogTitle> Web Application Development </ DialogTitle>  
-          <DialogContent> 
-             <DialogContentText>
-              <p> {helloUser} {theTime} and you're off to a good start! In this course, you will learn how to build web applications using React JavaScript and Google's Material UI Design. Here we'll cover the folowing basic concepts.
-              </ p> 
-           </ DialogContentText>
-              <div>
-                <a className="indent"> <b> List of topics: </ b> </ a>
-                <ul className="deepIndent">
-                  <li> Lesson 1: { topics.lesson1 }   </ li>
-                  <li> Lesson 2: { topics.lesson2 }   </ li>
-                  <li> Lesson 3: { topics.lesson3 }   </ li>
-                  <li> Lesson 4: { topics.lesson4 }   </ li>
-                  <li>  { topics.test }   </ li>
-                </ ul>
-              </ div>
-          </ DialogContent>
-          <DialogActions>
-            <Button color="primary" onClick={this.handleClose}> OK </ Button>
-          </ DialogActions>
-        </ Dialog>
-      <Typography variant="display1" gutterBottom> {user.level} React JavaScript App </ Typography>
-      <body>
-       <Typography variant="subheading" gutterBottom> Implementing Material UI Design components by Google in { new Date().getFullYear() } </ Typography>
-          <Button class="buttonIntro" style={{width: 140, height: 40}} variant="raised" onClick={this.handleClick} > Introduction </ Button>
-          <p> </ p>
-          <Button class="buttonLessonOne" style={{width: 120, height: 70}} variant="raised" onClick={ LessonOne } > Lesson 1 </ Button>
-          <a> </ a>
-          <Button class="buttonLessonTwo" style={{width: 120, height: 70}} variant="raised" onClick={ LessonTwo } > Lesson 2 </ Button> 
-          <p> </ p>
-          <Button class="buttonLessonThree" style={{width: 120, height: 70}} variant="raised" onClick={ LessonThree } > Lesson 3 </ Button>
-          <a> </ a>
-          <Button class="buttonLessonFour" style={{width: 120, height: 70}}  variant="raised" onClick={ LessonFour } > Lesson 4 </ Button>
-          <p> </ p>
-          <Button class="buttonTest" style={{width: 100, height: 40}} variant="raised" onClick={ Test } > Test </ Button>
-          <p> </ p>
-          <h4> In a hurry? Try quick search: </ h4>
-         <form>
-           <input
-             placeholder="Search for..."
-             ref={input => this.search = input}
-             onChange={ this.handleInputChange } />
-           <p> {this.state.query} </ p>
-         </ form>
-      </ body>
-        <Button class="buttonNext" style={{width: 130, height: 40}} variant="raised" href="courses.asp"> Next Course </ Button>
-      <div>
-          <h5> { footerCurrent } </ h5>
-        <Button class="buttonHelp" style={{width: 130, height: 40}} variant="raised" href="help.html"> { help } </ Button>
-      </ div>
-        </ div>
+    return (    
+            <div>
+              <div className={ classes.root }>
+                <Dialog open={ open } onClose={ this.handleClose }>
+                  <DialogTitle> Web Application Development </ DialogTitle>  
+                  <DialogContent> 
+                  <DialogContentText>
+                  <p> 
+                    {helloUser} {theTime} and you're off to a good start! In this course, you'll learn how to build web applications using React JavaScript and Google's Material UI Design. 
+                    Here we'll cover the folowing basic concepts.
+                  </ p> 
+                    </ DialogContentText>
+                      <div>
+                      <a className="indent"> <b> List of topics: </ b> </ a>
+                        <ul className="deepIndent"> 
+                        <li> <b> Lesson 1: </ b> { topics.lesson1 }   </ li>
+                        <li> <b> Lesson 2: </ b> { topics.lesson2 }   </ li>
+                        <li> <b> Lesson 3: </ b> { topics.lesson3 }   </ li>
+                        <li> <b> Lesson 4: </ b> { topics.lesson4 }   </ li>
+                        <li> <b> Test:  </ b> { topics.test }   </ li>
+                        </ ul>
+                      </ div>
+                    </ DialogContent>
+                  <DialogActions>
+                    <Button color="primary" onClick={this.handleClose}> OK </ Button>
+                  </ DialogActions>
+                </ Dialog>
+
+                  <div>
+                    <body>
+                    <Typography variant="display1" gutterBottom> <a> {user.level} React JavaScript App </ a> </ Typography>
+                    <Typography variant="subheading" gutterBottom> Implementing Material UI Design components by Google in { new Date().getFullYear() } </ Typography>
+                    <Button class="buttonIntro" style={{width: 140, height: 40}} variant="raised" onClick={this.handleClick} > Introduction </ Button>
+
+                      <p> </ p>
+                      <Button class="buttonLessonOne" style={{width: 120, height: 70}} variant="raised" onClick={ LessonOne } > Lesson 1 </ Button>
+                      <a> </ a>
+                      <Button class="buttonLessonTwo" style={{width: 120, height: 70}} variant="raised" onClick={ LessonTwo } > Lesson 2 </ Button> 
+                      <p> </ p>
+                      <Button class="buttonLessonThree" style={{width: 120, height: 70}} variant="raised" onClick={ LessonThree } > Lesson 3 </ Button>
+                      <a> </ a>
+                      <Button class="buttonLessonFour" style={{width: 120, height: 70}}  variant="raised" onClick={ LessonFour } > Lesson 4 </ Button>
+                      <p> </ p>
+                      <Button class="buttonTest" style={{width: 100, height: 40}} variant="raised" onClick={ Test } > Test </ Button>
+                      <p> </ p>
+
+
+                    <h4> In a hurry? Try quick search: </ h4>
+                      <form>
+                        <input
+                        placeholder="Search for..."
+                        ref={input => this.search = input}
+                        onChange={ this.handleInputChange } />
+                      <p> {this.state.query} </ p>
+                      </ form>
+                    </ body>
+                  </ div>
+
+                  <Button class="buttonNext" style={{width: 130, height: 40}} variant="raised" href="courses.asp"> Next Course </ Button>
+                  <div>
+                  <h5> { footerCurrent } </ h5>
+                <Button class="buttonHelp" style={{width: 130, height: 40}} variant="raised" href="help.html"> { help } </ Button>
+                </ div>
+              </ div> 
+            </ div>
+
     );
   }
 }
